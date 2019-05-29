bb62a44bad399f97ddc60624a2fef8ce416b8c14
kilian <kilualthaus@hotmail.com>
2019-05-29 14:44:54 +0200

    build teil commit | pfad anpassen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+const baseUrl = window.location.protocol + '//' + window.location.hostname +
+    (window.location.port ? ':' + window.location.port : '') + '/Practiframe/';
 export const environment = {
-  production: true
+    production: true,
+    baseUrl: baseUrl,
+    ajaxUrl: baseUrl + 'ajax/',
+    newAjax: baseUrl + 'angular_ajax',
 };
